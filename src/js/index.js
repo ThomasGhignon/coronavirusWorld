7af85ac50a7f69486c5269ad0577124a283e331c
ThomasGhignon <ghignon.thomas@gmail.com>
2020-03-13 09:11:11 +0100

    first push

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+import '../css/app.scss';
+/*import Background from './background';*/
+import TotalStat from './totalStat';
+class App {
+    constructor () {
+        this.initApp();
+    }
+
+    initApp () {
+      // Start application
+      /*new Background();*/
+      /*new TotalStat();*/
+    }
+}
+
+new App();
---
+++ src/js/totalStat.js
@@ -0,0 +1,45 @@
+import $ from 'jquery';
+
+
+
+export default class TotalStat{
+  constructor(){
+    this.initEls();
+    this.initEvents();
+  }
+
+  initEls(){
+    this.$els ={
+      totalConfirmed: $('.js-totalConfirmed'),
+      totalRecovered: $('.js-totalRecovered'),
+      totalDeaths: $('.js-totalDeaths')
+    }
+  }
+
+  initEvents(){
+    this.getTotalStat();
+  }
+
+  getTotalStat(){
+    const api = {
+      endpoint: 'https://corona.lmao.ninja/all',
+    };
+    $.ajaxSetup({cache:false});
+
+    $.getJSON(api.endpoint)
+      .then((response) =>{
+        console.log(response);
+        console.log(response.cases);
+        this.renderTotalStat(response.cases, response.deaths, response.recovered);
+      })
+      .catch((e) =>{
+        console.log('error with the quote :', e);
+      });
+  }
+  renderTotalStat(cases, deaths, recovered){
+      this.$els.totalConfirmed.text(cases);
+      this.$els.totalRecovered.text(recovered);
+      this.$els.totalDeaths.text(deaths);
+      /*this.$els.main.addClass('is-ready');*/
+    }
+  }
